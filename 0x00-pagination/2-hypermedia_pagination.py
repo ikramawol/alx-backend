4de2063f1b2ab106d2d5b265cb397b2a3b6af888
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 <ikruawol.14@gmail.com>
2023-02-19 00:47:41 -0800

    padination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""
+Module 1-simple_pagination
+"""
 
 import csv
 import math
-from typing import Dict, List
-
-index_range = __import__('0-simple_helper_function').index_range
+from typing import List, Tuple
 
 
 class Server:
@@ -26,44 +27,67 @@ class Server:
 
         return self.__dataset
 
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """
+        Parameters
+        ---------
+        page: int
+            the start of pagination
+        page_size: int
+            the maximum number of object returned
+
+        Returns
+        -------
+        Tuple
+            tuple holding the beginning and ending page numbers
+        """
+        end_index = page * page_size
+        start_index = end_index - page_size
+        return (start_index, end_index)
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Takes 2 args and returns the requested page number
-        page: the requested page number
-        page_size: number of records per page
-        both params must be int and > 0
-        """
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+        Parameters
+        ---------
+        page: int
+            the start of pagination
+        page_size: int
+            the maximum number of object returned
 
+        Returns
+        -------
+        list
+            the appropriate page of the dataset or the correct list of row
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        result = self.index_range(page, page_size)
         data = self.dataset()
-
-        try:
-            idx = index_range(page, page_size)
-            return data[idx[0]:idx[1]]
-        except IndexError:
+        if len(data) < result[0]:
             return []
+        return data[result[0]: result[1]]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-            page_size: the length of the returned data set page
-            page: the current page number
-            data: the dataset page
-            next_page: number of the next page, None if no page
-            prev_page: number of the previous page, None if no page
-            total_pages: total number of pages in the data set
-            return a dict of the above info
-        """
-        total_pages = (len(self.dataset()) // page_size) + 1
+        Parameters
+        ---------
+        page: int
+            the start of pagination
+        page_size: int
+            the maximum number of object returned
 
+        Returns
+        -------
+        dict
+            the appropriate page of the dataset or the correct list of row
+        """
+        total_pages = math.ceil(len(self.dataset()) / page_size)
         data = self.get_page(page, page_size)
-
-        dict_t = {
-            'page_size': page_size if page_size <= len(data) else len(data),
-            'page': page,
-            'data': data,
-            'next_page': page + 1 if page + 1 <= total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages
-        }
-        return dict_t
+        new_dict = {"page_size": len(data) if page_size >= len(data)
+                    else page_size,
+                    "page": page,
+                    "data": data,
+                    "next_page": page + 1 if page + 1 <= total_pages else None,
+                    "prev_page": page - 1 if page > 1 else None,
+                    "total_pages": total_pages}
+        return new_dict
